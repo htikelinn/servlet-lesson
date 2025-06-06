d357b1e4dffceb66033d8078e0d08896775f0201
htikelinn <htikezawlinn@gmail.com>
2025-06-06 14:34:53 +0630

    add doGet code

---
+++ src/main/java/com/dos/hzl/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.dos.hzl;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//req = request and resp = response
+		resp.getWriter().append("Hello Servlet");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
