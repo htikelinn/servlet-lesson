6a497828a5cf95018d0903200bbd1a7f023fa745
htikelinn <htikezawlinn@gmail.com>
2025-06-06 14:36:56 +0630

    add annotation code WebServlet to code

--- src/main/java/com/dos/hzl/HelloServlet.java
+++ src/main/java/com/dos/hzl/HelloServlet.java
@@ -3,10 +3,16 @@ package com.dos.hzl;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+@WebServlet(urlPatterns = {
+		"/hello",
+		"/hello-world"
+})
 public class HelloServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
